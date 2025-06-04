f56771a18aa1f64a786e18e037aa45e3c5539071
simon.fisher <simon.fisher@mre.medical.canon>
2025-06-04 17:09:16 +0100

    Adding assert to catch missing samples for each dataset

--- SRC/Runnable/run_preprocess.py
+++ SRC/Runnable/run_preprocess.py
@@ -32,12 +32,58 @@ def run_preprocesing():
     train_mutation, val_mutation, test_mutation = mutation_preprocess.mutation_preprocess(df_mutation, manifest)
 
     # print modality shapes
-    logging.info(f"Processed Methylation Data: Train {train_meth.shape}, Val {val_meth.shape}, Test {test_meth.shape}")
-    logging.info(f"Processed miRNA Data: Train {train_mir.shape}, Val {val_mir.shape}, Test {test_mir.shape}")
-    logging.info(f"Processed RNA Data: Train {train_rna.shape}, Val {val_rna.shape}, Test {test_rna.shape}")
-    logging.info(f"Processed CNV Data: Train {train_cnv.shape}, Val {val_cnv.shape}, Test {test_cnv.shape}")
-    logging.info(f"Processed Mutation Data: Train {train_mutation.shape}, Val {val_mutation.shape}, Test {test_mutation.shape}")
+    #get the intersection of all submitter_id.samples for train, val, and test sets
 
+    train_intersection_ids = set(train_meth['submitter_id.samples']).intersection(
+        set(train_mir['submitter_id.samples']),
+        set(train_rna['submitter_id.samples']),
+        set(train_cnv['submitter_id.samples']),
+        set(train_mutation['submitter_id.samples'])
+    )
+
+    val_intersection_ids = set(val_meth['submitter_id.samples']).intersection(
+        set(val_mir['submitter_id.samples']),
+        set(val_rna['submitter_id.samples']),
+        set(val_cnv['submitter_id.samples']),
+        set(val_mutation['submitter_id.samples'])
+    )
+
+    test_intersection_ids = set(test_meth['submitter_id.samples']).intersection(
+        set(test_mir['submitter_id.samples']),
+        set(test_rna['submitter_id.samples']),
+        set(test_cnv['submitter_id.samples']),
+        set(test_mutation['submitter_id.samples'])
+    )
+
+    logging.info(f"Train set intersection IDs: {len(train_intersection_ids)}")
+    logging.info(f"Validation set intersection IDs: {len(val_intersection_ids)}")
+    logging.info(f"Test set intersection IDs: {len(test_intersection_ids)}")
+
+    #filter the dataframes to only include the intersection IDs
+    train_meth = train_meth[train_meth['submitter_id.samples'].isin(train_intersection_ids)]
+    val_meth = val_meth[val_meth['submitter_id.samples'].isin(val_intersection_ids)]
+    test_meth = test_meth[test_meth['submitter_id.samples'].isin(test_intersection_ids)]
+
+    train_mir = train_mir[train_mir['submitter_id.samples'].isin(train_intersection_ids)]
+    val_mir = val_mir[val_mir['submitter_id.samples'].isin(val_intersection_ids)]
+    test_mir = test_mir[test_mir['submitter_id.samples'].isin(test_intersection_ids)]
+
+    train_rna = train_rna[train_rna['submitter_id.samples'].isin(train_intersection_ids)]
+    val_rna = val_rna[val_rna['submitter_id.samples'].isin(val_intersection_ids)]
+    test_rna = test_rna[test_rna['submitter_id.samples'].isin(test_intersection_ids)]
+
+    train_cnv = train_cnv[train_cnv['submitter_id.samples'].isin(train_intersection_ids)]
+    val_cnv = val_cnv[val_cnv['submitter_id.samples'].isin(val_intersection_ids)]
+    test_cnv = test_cnv[test_cnv['submitter_id.samples'].isin(test_intersection_ids)]
+
+    train_mutation = train_mutation[train_mutation['submitter_id.samples'].isin(train_intersection_ids)]
+    val_mutation = val_mutation[val_mutation['submitter_id.samples'].isin(val_intersection_ids)]
+    test_mutation = test_mutation[test_mutation['submitter_id.samples'].isin(test_intersection_ids)]
+
+    #assert that the number of samples in each modality is the same
+    assert len(train_meth) == len(train_mir) == len(train_rna) == len(train_cnv) == len(train_mutation), "Train sets do not match in size"
+    assert len(val_meth) == len(val_mir) == len(val_rna) == len(val_cnv) == len(val_mutation), "Validation sets do not match in size"
+    assert len(test_meth) == len(test_mir) == len(test_rna) == len(test_cnv) == len(test_mutation), "Test sets do not match in size"
 
     #save to ../../processed_data/
     train_meth.to_csv("../../processed_data/train_meth.csv")
