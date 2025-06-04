8111beca822d6610bbb2861fd6e4b1725bff9699
ekansh09 <echauhan09@gmail.com>
2025-06-04 19:08:45 +0100

    H&E segmentation, patching and feature extraction

---
+++ SRC/packages/CLAM/utils/constants.py
@@ -0,0 +1,48 @@
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+OPENAI_MEAN = [0.48145466, 0.4578275, 0.40821073]
+OPENAI_STD = [0.26862954, 0.26130258, 0.27577711]
+H_optimus_MEAN = [0.707223, 0.578729, 0.703617]
+H_optimus_STD = [0.211883, 0.230117, 0.177517]
+
+MODEL2CONSTANTS = {
+	"resnet50_trunc": {
+		"mean": IMAGENET_MEAN,
+		"std": IMAGENET_STD
+	},
+	"uni_v1":
+	{
+		"mean": IMAGENET_MEAN,
+		"std": IMAGENET_STD
+	},
+	"uni_v2":
+	{
+		"mean": IMAGENET_MEAN,
+		"std": IMAGENET_STD
+	},
+	"conch_v1":
+	{
+		"mean": OPENAI_MEAN,
+		"std": OPENAI_STD
+	},
+    "conch_v1_5":
+    {
+		"mean": IMAGENET_MEAN,
+		"std": IMAGENET_STD
+	},
+	"hoptimus":
+	{
+		"mean": H_optimus_MEAN,
+		"std": H_optimus_STD
+	},
+		"hoptimus":
+	{
+		"mean": H_optimus_MEAN,
+		"std": H_optimus_STD
+	},
+	"virchow_2":
+	{
+		"mean": IMAGENET_MEAN,
+		"std": IMAGENET_STD
+	},
+}
