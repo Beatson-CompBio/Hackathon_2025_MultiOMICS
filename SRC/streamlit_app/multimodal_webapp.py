91b9d5ccf1b150126b7556d1962b6d1d835addf9
farzaneh <farzaneh.shahi1375@gmail.com>
2025-06-06 13:52:13 +0100

    logo updated

--- SRC/streamlit_app/multimodal_webapp.py
+++ SRC/streamlit_app/multimodal_webapp.py
@@ -267,8 +267,12 @@ class LateIntegrationModel:
 
 # --- Streamlit Interface with Tabs ---
 st.set_page_config(page_title="Multimodal Model App", layout="wide")
-st.title('🎗️ BreaSight: A Deep Dive into Lobular & Ductal Cancer')
+col1, col2 = st.columns([1, 4])
+with col1:
+    st.image("logo_updated.png", width=220)
 
+with col2:
+    st.markdown("""<div style="height: 220px; display: flex; align-items: center;"><h1 style="margin: 0;">BreaSight: A Deep Dive into Lobular & Ductal Cancer</h1></div>""",unsafe_allow_html=True,)
 
 # === CACHE DATA LOADER ===
 @st.cache_data
